26f38769a6f0e551dac0035d78cfba423fc5311b
gly <glefranc@eig.fr>
2023-02-01 15:02:32 +0100

    add test_mmg

---
+++ test_mmg/xmake.lua
@@ -0,0 +1,9 @@
+add_repositories("test-xmake-repo test-xmake-repo")
+add_requires("mmg")
+
+target("test-mmg")
+    set_kind("binary")
+    set_languages("cxx20")
+    add_files("src/main.cpp")
+    add_files("args.glap.yml")
+    add_rules("@mmg/mmg")
